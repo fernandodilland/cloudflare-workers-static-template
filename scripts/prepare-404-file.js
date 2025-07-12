5379c718c2141b18590b65d10b4eb1ee718c68bb
Fernando <fernandodilland@gmail.com>
2025-07-12 14:10:56 -0600

    Commit

---
+++ scripts/prepare-404-file.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const srcDir = path.join(__dirname, '../src');
+const html404 = path.join(srcDir, '404.html');
+const folder404 = path.join(srcDir, '404');
+const folder404Index = path.join(folder404, 'index.html');
+const folder404Html = path.join(folder404, '404.html');
+
+try {
+  if (!fs.existsSync(html404)) {
+    if (fs.existsSync(folder404Index)) {
+      // Rename index.html to 404.html inside the folder
+      fs.renameSync(folder404Index, folder404Html);
+      // Move 404.html to src/
+      fs.renameSync(folder404Html, html404);
+      // Remove the 404 folder
+      fs.rmdirSync(folder404, { recursive: true });
+      console.log('404.html preparado correctamente.');
+    } else {
+      console.log('No se encontró src/404/index.html, no se realizaron cambios.');
+    }
+  } else {
+    console.log('src/404.html ya existe, no se realizaron cambios.');
+  }
+} catch (err) {
+  console.error('Error preparando 404.html:', err);
+  process.exit(1);
+}
